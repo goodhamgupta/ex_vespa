271a5980eb39752b8e76c2b3537e9d11d59e0e91
Shubham Gupta <shubhamg2208@live.com>
2022-07-05 23:38:44 +0800

    Add repo template

---
+++ lib/ex_vespa.ex
@@ -0,0 +1,18 @@
+defmodule ExVespa do
+  @moduledoc """
+  Documentation for `ExVespa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExVespa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_vespa_test.exs
@@ -0,0 +1,8 @@
+defmodule ExVespaTest do
+  use ExUnit.Case
+  doctest ExVespa
+
+  test "greets the world" do
+    assert ExVespa.hello() == :world
+  end
+end
