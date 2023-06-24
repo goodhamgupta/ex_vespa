9ea2d796ca5a33fdba92064f1f876594532e02b7
Shubham Gupta <shubhamg2208@live.com>
2023-06-24 16:18:27 +0800

    Broke some tests

--- lib/ex_vespa/templates/schema.ex
+++ lib/ex_vespa/templates/schema.ex
@@ -7,4 +7,13 @@ defmodule ExVespa.Templates.Schema do
     "#{File.cwd!()}/lib/ex_vespa/templates/schema.eex",
     [:schema]
   )
+
+  def construct_struct_field(sf) do
+    out = """
+    struct-field #{sf.name} {
+    }
+    """
+
+    EEx.eval_string(out, list: [sf])
+  end
 end
