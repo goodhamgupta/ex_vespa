1003b2a124c826bd184b74ab52abb80c0061d7c8
goodhamgupta <shubhamg2208@live.com>
2023-07-23 23:20:55 +0800

    Add implementation for VespaSync class

--- lib/ex_vespa/application/vespa_sync.ex
+++ lib/ex_vespa/application/vespa_sync.ex
@@ -5,6 +5,7 @@ defmodule ExVespa.Application.VespaSync do
 
   alias __MODULE__
   alias ExVespa.Application.Vespa
+  alias ExVespa.IO.VespaResponse
 
   @keys [
     :app,
@@ -73,12 +74,14 @@ defmodule ExVespa.Application.VespaSync do
   def feed_data_point(%VespaSync{} = vespa_sync, schema, data_id, fields, namespace \\ nil) do
     base_url = "#{vespa_sync.app.end_point}/document/v1"
 
-    endpoint =
-      if namespace != nil do
-        "#{base_url}/#{namespace}/#{schema}/docid/#{data_id}"
+    cur_namespace =
+      if namespace == nil do
+        schema
       else
-        "#{base_url}/#{schema}/docid/#{data_id}"
+        namespace
       end
+
+    endpoint = "#{base_url}/#{cur_namespace}/#{schema}/docid/#{data_id}"
 
     req_headers = [{"Content-Type", "application/json"}]
 
@@ -92,7 +95,170 @@ defmodule ExVespa.Application.VespaSync do
       )
 
     if status_code == 200 do
-      {:ok, Jason.encode!(response)}
+      {:ok,
+       %VespaResponse{
+         json: Jason.encode!(response),
+         status_code: status_code,
+         url: endpoint,
+         operation_type: "feed"
+       }}
+    else
+      {:error, response}
+    end
+  end
+
+  def query(%VespaSync{} = vespa_sync, body \\ %{}) do
+    # Make post request to search_end_point
+    {:ok, {{http_method, status_code, msg}, headers, response}} =
+      :httpc.request(
+        :post,
+        {vespa_sync.app.search_end_point, []},
+        [],
+        [{:ssl, [{:certfile, vespa_sync.cert}]}],
+        Jason.encode!(body)
+      )
+
+    {:ok,
+     %VespaResponse{
+       json: Jason.encode!(response),
+       status_code: status_code,
+       url: vespa_sync.app.search_end_point,
+       operation_type: "query"
+     }}
+  end
+
+  def delete_data(%VespaSync{} = vespa_sync, schema, data_id, namespace \\ nil) do
+    base_url = "#{vespa_sync.app.end_point}/document/v1"
+
+    cur_namespace =
+      if namespace == nil do
+        schema
+      else
+        namespace
+      end
+
+    endpoint = "#{base_url}/#{cur_namespace}/#{schema}/docid/#{data_id}"
+
+    {:ok, {{http_method, status_code, msg}, headers, response}} =
+      :httpc.request(
+        :delete,
+        {endpoint, []},
+        [],
+        [{:ssl, [{:certfile, vespa_sync.cert}]}]
+      )
+
+    if status_code == 200 do
+      {:ok,
+       %VespaResponse{
+         json: Jason.encode!(response),
+         status_code: status_code,
+         url: endpoint,
+         operation_type: "delete"
+       }}
+    else
+      {:error, response}
+    end
+  end
+
+  def delete_all_docs(%VespaSync{} = vespa_sync, content_cluster_name, schema, namespace \\ nil) do
+    base_url = "#{vespa_sync.app.end_point}/document/v1"
+
+    cur_namespace =
+      if namespace == nil do
+        schema
+      else
+        namespace
+      end
+
+    endpoint =
+      "#{base_url}/#{cur_namespace}/#{schema}/docid/?cluster=#{content_cluster_name}&selection=true"
+
+    {:ok, {{http_method, status_code, msg}, headers, response}} =
+      :httpc.request(
+        :delete,
+        {endpoint, []},
+        [],
+        [{:ssl, [{:certfile, vespa_sync.cert}]}]
+      )
+
+    if status_code == 200 do
+      {:ok,
+       %VespaResponse{
+         json: Jason.encode!(response),
+         status_code: status_code,
+         url: endpoint,
+         operation_type: "delete"
+       }}
+    else
+      {:error, response}
+    end
+  end
+
+  def get_data(%VespaSync{} = vespa_sync, schema, data_id, namespace \\ nil) do
+    base_url = "#{vespa_sync.app.end_point}/document/v1"
+
+    cur_namespace =
+      if namespace == nil do
+        schema
+      else
+        namespace
+      end
+
+    endpoint = "#{base_url}/#{cur_namespace}/#{schema}/docid/#{data_id}"
+
+    {:ok, {{http_method, status_code, msg}, headers, response}} =
+      :httpc.request(
+        :get,
+        {endpoint, []},
+        [],
+        [{:ssl, [{:certfile, vespa_sync.cert}]}]
+      )
+
+    if status_code == 200 do
+      {:ok,
+       %VespaResponse{
+         json: Jason.encode!(response),
+         status_code: status_code,
+         url: endpoint,
+         operation_type: "get"
+       }}
+    else
+      {:error, response}
+    end
+  end
+
+  def update_data(%VespaSync{} = vespa_sync, schema, data_id, fields, namespace \\ nil) do
+    base_url = "#{vespa_sync.app.end_point}/document/v1"
+
+    cur_namespace =
+      if namespace == nil do
+        schema
+      else
+        namespace
+      end
+
+    endpoint = "#{base_url}/#{cur_namespace}/#{schema}/docid/#{data_id}"
+
+    req_headers = [{"Content-Type", "application/json"}]
+    vespa_format = %{fields: Enum.map(fields, fn {k, v} -> {k, %{assign: v}} end)}
+
+    {:ok, {{http_method, status_code, msg}, headers, response}} =
+      :httpc.request(
+        :put,
+        {endpoint, req_headers},
+        [],
+        [{:ssl, [{:certfile, vespa_sync.cert}]}],
+        Jason.encode!(vespa_format)
+      )
+
+    if status_code == 200 do
+      {:ok,
+       %VespaResponse{
+         json: Jason.encode!(response),
+         status_code: status_code,
+         url: endpoint,
+         operation_type: "update"
+       }}
     else
       {:error, response}
     end
